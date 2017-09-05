954e95408a5cd63932b67c3d13cb70a7afcb7bce
david friedman <mrdavidfriedman@gmail.com>
2017-09-05 16:17:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function increaseRankBy(n) {
     let children = rankedLists[i].children;
 
     for (let j=0; j < children.length; j++) {
-      children[j].innerHTML = parseInt(children[i].innerHTML)+n;
+      children[j].innerHTML = parseInt(children[j].innerHTML)+n;
     }
   }
 }
